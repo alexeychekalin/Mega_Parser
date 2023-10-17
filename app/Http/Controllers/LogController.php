a3ffbf44418a4c98490bbd8dbf078d90ea110033
alexeychekalin <alexey.chekalin@gmail.com>
2023-10-17 17:43:42 +0300

    v1.7.1 - Сообщение о повторном запуске, мелкие правки

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -18,7 +18,7 @@ class LogController extends Controller
     public function readLogs(Request $request)
     {
         if($request['tab'] == 'one')
-            $data = file('SberParseApi.log');
+            $data = file('/home/mega_parser/log/SberParseApi.log');
         else
             $data = file('/home/mega_parser/log/TelegramApi.log');
         $data = array_reverse($data);
